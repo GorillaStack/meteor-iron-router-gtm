87c946a1ce85a9ca25df59717acaa4867deb1694
Elliott Spira <elliottspira@gmail.com>
2015-08-22 15:45:23 +1000

    working version with gtm insertion and with iron router hook

--- package.js
+++ package.js
@@ -1,18 +1,31 @@
 Package.describe({
   name: 'gorillastack:iron-router-gtm',
-  version: '0.0.1',
+  version: '0.1.0',
   summary: 'Install google tag manager and track pageviews in iron-router',
   git: 'git@github.com:GorillaStack/meteor-iron-router-gtm.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
-  api.addFiles('iron-router-gtm.js');
+  api.versionsFrom('METEOR@1.0.1');
+  api.use([
+    'templating',
+    'iron:router@1.0.0'
+  ], 'client');
+
+  api.addFiles([
+    'lib/client/body.html',
+    'lib/client/attach_google_tag_manager.js',
+    'lib/client/router.js'
+  ], 'client');
+
+  api.addFiles([
+    'lib/server/set_browser_policy.js'
+  ], 'server');
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('gorillastack:iron-router-gtm');
-  api.addFiles('iron-router-gtm-tests.js');
-});
+// Package.onTest(function(api) {
+//   api.use('tinytest');
+//   api.use('gorillastack:iron-router-gtm');
+//   api.addFiles('iron-router-gtm-tests.js');
+// });
